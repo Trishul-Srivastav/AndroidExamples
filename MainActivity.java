360450dd9f45d636c032ee950666e69d1f32dbdc
Trishul <trishulsrivastav97@gmail.com>
2018-07-22 12:32:00 +0530

    hjgkjhj

--- MainActivity.java
+++ MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_start,btn_stop;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
